e77393a102687a5d54405d52b6bc50ffa08738d5
thomas1424 <panthomas1424@gmail.com>
2025-01-26 16:38:11 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -18,19 +18,32 @@ let modalWpm = document.getElementById('modal-wpm');
 let modalAccuracy = document.getElementById('modal-accuracy');
 
 const texts = {
-    easy: ["The quick brown fox jumps over the lazy dog."],
-    medium: ["A journey of a thousand miles begins with a single step."],
-    hard: ["To be, or not to be, that is the question: Whether 'tis nobler in the mind to suffer."]
+    easy: [
+        "The quick brown fox jumps over the lazy dog.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "She sells seashells by the seashore."
+    ],
+    medium: [
+        "A journey of a thousand miles begins with a single step.",
+        "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.",
+        "You must be the change you wish to see in the world."
+    ],
+    hard: [
+        "To be, or not to be, that is the question: Whether 'tis nobler in the mind to suffer.",
+        "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.",
+        "In the beginning God created the heavens and the earth."
+    ]
 };
 
 startBtn.addEventListener('click', startTest);
 restartBtn.addEventListener('click', restartTest);
 themeToggle.addEventListener('click', toggleTheme);
 closeModalBtn.addEventListener('click', closeModal);
+inputArea.addEventListener('keypress', handleEnterPress);
 
 function startTest() {
     const difficulty = difficultySelect.value;
-    textToType = texts[difficulty][0];
+    textToType = getRandomText(difficulty);
     document.getElementById('text-to-type').innerText = textToType;
     inputArea.disabled = false;
     inputArea.value = '';
@@ -68,6 +81,20 @@ function handleInput() {
         inputArea.disabled = true;
         displayResultsModal();
     }
+}
+
+function handleEnterPress(event) {
+    if (event.key === 'Enter') {
+        clearInterval(timer);
+        inputArea.disabled = true;
+        displayResultsModal();
+    }
+}
+
+function getRandomText(difficulty) {
+    const textsArray = texts[difficulty];
+    const randomIndex = Math.floor(Math.random() * textsArray.length);
+    return textsArray[randomIndex];
 }
 
 function countCorrectChars(typedText, referenceText) {
